e5f892de3f24570b65e523bbb301c98d7e340a4b
croniarc <croniarc@gmail.com>
2024-03-08 00:44:33 +0530

    updated code dynamic button added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
-import React,{useState} from 'react'
+import { useState } from 'react'
 import "./App.css";
+import Button from './Component/Button/Button';
 const App = () => {
-  const[count,setCount]=useState(0);
+  const [count, setCount] = useState(0);
+  const incrementFunc = () => {
+    setCount(count + 1);
+  }
+  const decrementFunc = () => {
+    setCount(count - 1);
+  }
   return (
     <div className='app'>
       <h1>Increment decrement counter</h1>
       <div className="box">
-       <button onClick={() =>setCount(count+1)}>incremnet</button>
-       <h1>{count}</h1>
-       <button onClick={() =>setCount(count-1)}>decrement</button>
-
+        <Button name={"increment"} buttonFunc={incrementFunc}/>
+        <h1>{count}</h1>
+        <Button name={"decrement"} buttonFunc={decrementFunc}/>
+        <Button name={"search"} buttonFunc={''}/>
       </div>
     </div>
   )
---
+++ src/Component/Button/Button.jsx
@@ -0,0 +1,14 @@
+// import  from 'react'
+import './button.css'
+const Button = (props) => {
+    const name = props?.name; 
+    const buttonFunc = props?.buttonFunc;
+    console.log(props)
+    return (
+        <div onClick={()=> buttonFunc()} className='button'>
+            {name}
+        </div>
+    )
+}
+
+export default Button
